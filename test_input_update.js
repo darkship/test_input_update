caaf6d8f79eac9442a26539d32dca57e876af37a
tkraebihl <tkraebihl@tiz.fr>
2014-07-03 12:00:03 +0200

    init

---
+++ test_input_update.js
@@ -0,0 +1,29 @@
+text = new Meteor.Collection("text")
+
+if (Meteor.isClient) {
+    Template.hello.helpers({
+        greeting: function () {
+            return text.findOne({_id: "123"})
+        }
+    });
+    Template.hello.events({
+        'keyup input':function(){
+
+        },
+        'keydown .keydown': function (evt, tpl) {
+            console.log("here")
+            text.update(this._id, {$set: {mytext: evt.currentTarget.value}})
+        },
+        'keyup .keyup': function (evt, tpl) {
+            // template data, if any, is available in 'this'
+            text.update(this._id, {$set: {mytext: evt.currentTarget.value}})
+        }
+    });
+}
+
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+        if (!text.findOne({_id: "123"}))
+            text.insert({_id: "123", mytext: "new text"})
+    });
+}
