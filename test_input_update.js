76ed87513676f8bf6b9810de7fc9ac05b34108ba
tkraebihl <tkraebihl@tiz.fr>
2014-10-21 15:34:30 +0200

    update 9.4

--- test_input_update.js
+++ test_input_update.js
@@ -7,15 +7,17 @@ if (Meteor.isClient) {
         }
     });
     Template.hello.events({
-        'keyup input':function(){
-
-        },
         'keydown .keydown': function (evt, tpl) {
             console.log("here")
             text.update(this._id, {$set: {mytext: evt.currentTarget.value}})
         },
         'keyup .keyup': function (evt, tpl) {
             // template data, if any, is available in 'this'
+            text.update(this._id, {$set: {mytext: evt.currentTarget.value}})
+        },
+        'keydpress .keydpress': function (evt, tpl) {
+            // template data, if any, is available in 'this'
+            console.log("keypress")
             text.update(this._id, {$set: {mytext: evt.currentTarget.value}})
         }
     });
